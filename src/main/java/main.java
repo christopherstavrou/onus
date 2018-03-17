56d18180faad7118b56f61e5f23fb33208e6a570
Christopher Stavrou <christopherstavrou@gmail.com>
2018-03-17 16:31:59 +0000

    master: create initial files. MainContainer.fxml and mainTest are temporary and might be deleted in future.

---
+++ src/main/java/main.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Main class for onus the task desktop overlay application
+ *
+ * @author Christopher Stavrou
+ */
+public class main extends Application {
+
+    /**
+     * Main runnable
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
